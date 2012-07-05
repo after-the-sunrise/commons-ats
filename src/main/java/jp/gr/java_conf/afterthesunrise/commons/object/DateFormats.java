2a3688383afba94061aeef16ae0bfbb3e8cd7287
Takanori Takase <subac1008-git@yahoo.co.jp>
2012-07-06 00:23:15 +0900

    Added cached utilities.

---
+++ src/main/java/jp/gr/java_conf/afterthesunrise/commons/object/DateFormats.java
@@ -0,0 +1,116 @@
+package jp.gr.java_conf.afterthesunrise.commons.object;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+/**
+ * @author takanori.takase
+ */
+public final class DateFormats {
+
+	private DateFormats() {
+		throw new IllegalAccessError("Utility class shouldn't be instantiated.");
+	}
+
+	private static final int MAX = 20;
+
+	private static final long DURATION = 1L;
+
+	private static final TimeUnit UNIT = TimeUnit.MINUTES;
+
+	private static final ThreadLocal<LoadingCache<String, DateFormat>> LOCAL;
+
+	static {
+
+		final CacheLoader<String, DateFormat> loader = new CacheLoader<String, DateFormat>() {
+			@Override
+			public DateFormat load(String key) throws Exception {
+				try {
+					return new SimpleDateFormat(key);
+				} catch (Exception e) {
+					return null;
+				}
+			}
+		};
+
+		LOCAL = new ThreadLocal<LoadingCache<String, DateFormat>>() {
+			@Override
+			protected LoadingCache<String, DateFormat> initialValue() {
+
+				return CacheBuilder.newBuilder()
+						.expireAfterAccess(DURATION, UNIT).maximumSize(MAX)
+						.build(loader);
+
+			}
+		};
+
+	}
+
+	public static DateFormat get(String format) {
+
+		if (StringUtils.isBlank(format)) {
+			return null;
+		}
+
+		DateFormat raw = getRaw(format);
+
+		if (raw == null) {
+			return null;
+		}
+
+		// Defensive copy
+		return (DateFormat) raw.clone();
+
+	}
+
+	private static DateFormat getRaw(String format) {
+
+		if (StringUtils.isBlank(format)) {
+			return null;
+		}
+
+		return LOCAL.get().getUnchecked(format.intern());
+
+	}
+
+	public static Date format(String pattern, String value) {
+		return format(pattern, value, null);
+	}
+
+	public static Date format(String format, String value, TimeZone timeZone) {
+
+		if (StringUtils.isBlank(value)) {
+			return null;
+		}
+
+		DateFormat dateFormat = getRaw(format);
+
+		if (dateFormat == null) {
+			return null;
+		}
+
+		if (timeZone == null) {
+			dateFormat.setTimeZone(TimeZone.getDefault());
+		} else {
+			dateFormat.setTimeZone(timeZone);
+		}
+
+		try {
+			return dateFormat.parse(value);
+		} catch (ParseException e) {
+			return null;
+		}
+
+	}
+
+}
---
+++ src/main/java/jp/gr/java_conf/afterthesunrise/commons/object/Patterns.java
@@ -0,0 +1,72 @@
+package jp.gr.java_conf.afterthesunrise.commons.object;
+
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.google.common.base.Objects;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+/**
+ * @author takanori.takase
+ */
+public final class Patterns {
+
+	private Patterns() {
+		throw new IllegalAccessError("Utility class shouldn't be instantiated.");
+	}
+
+	private static final int MAX = 100;
+
+	private static final long DURATION = 1L;
+
+	private static final TimeUnit UNIT = TimeUnit.MINUTES;
+
+	private static final LoadingCache<String, Pattern> CACHE;
+
+	static {
+
+		CacheLoader<String, Pattern> loader = new CacheLoader<String, Pattern>() {
+			@Override
+			public Pattern load(String key) throws Exception {
+				try {
+					return Pattern.compile(key);
+				} catch (Exception e) {
+					return null;
+				}
+			}
+		};
+
+		CACHE = CacheBuilder.newBuilder().expireAfterAccess(DURATION, UNIT)
+				.maximumSize(MAX).build(loader);
+
+	}
+
+	public static Pattern compile(String pattern) {
+
+		if (pattern == null) {
+			return null;
+		}
+
+		return CACHE.getUnchecked(pattern.intern());
+
+	}
+
+	public static boolean match(String pattern, String value) {
+
+		Pattern p = compile(pattern);
+
+		if (p == null) {
+			return false;
+		}
+
+		String target = Objects.firstNonNull(value, StringUtils.EMPTY);
+
+		return p.matcher(target).matches();
+
+	}
+
+}
