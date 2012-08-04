ce97f334038224a36f7115edd76d616aac7cbf3b
Takanori Takase <subac1008-git@yahoo.co.jp>
2012-08-04 10:35:34 +0900

    Delegated methods to common interface.

--- src/main/java/jp/gr/java_conf/afterthesunrise/commons/executor/FixedExecutor.java
+++ src/main/java/jp/gr/java_conf/afterthesunrise/commons/executor/FixedExecutor.java
@@ -1,14 +1,13 @@
 package jp.gr.java_conf.afterthesunrise.commons.executor;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Future;
 
 /**
  * @author takanori.takase
  */
-public interface FixedExecutor extends AutoCloseable {
-
-	void execute(Runnable runnable);
+public interface FixedExecutor extends AutoCloseable, Executor {
 
 	<V> Future<V> execute(Callable<V> callable);
 
