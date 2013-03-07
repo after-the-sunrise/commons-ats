fd737e2273ded5827cf79efacaef3391495f25b5
Takanori Takase <subac1008-git@yahoo.co.jp>
2013-03-07 23:47:11 +0900

    Added basis and percent functions.

--- src/test/java/jp/gr/java_conf/afterthesunrise/commons/object/FunctionsTest.java
+++ src/test/java/jp/gr/java_conf/afterthesunrise/commons/object/FunctionsTest.java
@@ -1,12 +1,14 @@
 package jp.gr.java_conf.afterthesunrise.commons.object;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -105,6 +107,84 @@ public class FunctionsTest {
 		assertTrue(output.contains("1.1"));
 
 		assertTrue(output.contains(null));
+
+	}
+
+	@Test
+	public void test_TO_STRING() {
+
+		List<String> input = Arrays.asList("foo", null);
+
+		Function<Object, String> f = Functions.TO_STRING;
+
+		Collection<String> output = Lists.transform(input, f);
+
+		assertEquals(2, output.size());
+
+		assertTrue(output.contains("foo"));
+
+		assertTrue(output.contains(null));
+
+	}
+
+	@Test
+	public void test_TO_NAME() {
+
+		List<RoundingMode> input = Arrays.asList(RoundingMode.DOWN, null);
+
+		Function<Enum<?>, String> f = Functions.TO_NAME;
+
+		Collection<String> output = Lists.transform(input, f);
+
+		assertEquals(2, output.size());
+
+		assertTrue(output.contains("DOWN"));
+
+		assertTrue(output.contains(null));
+
+	}
+
+	@Test
+	public void test_REAL_PERCENT() {
+
+		Function<BigDecimal, BigDecimal> f = Functions.REAL_PERCENT;
+
+		assertNull(f.apply(null));
+
+		assertEquals(new BigDecimal("1000"), f.apply(BigDecimal.TEN));
+
+	}
+
+	@Test
+	public void test_PERCENT_REAL() {
+
+		Function<BigDecimal, BigDecimal> f = Functions.PERCENT_REAL;
+
+		assertNull(f.apply(null));
+
+		assertEquals(new BigDecimal("0.10"), f.apply(BigDecimal.TEN));
+
+	}
+
+	@Test
+	public void test_REAL_BASIS() {
+
+		Function<BigDecimal, BigDecimal> f = Functions.REAL_BASIS;
+
+		assertNull(f.apply(null));
+
+		assertEquals(new BigDecimal("100000"), f.apply(BigDecimal.TEN));
+
+	}
+
+	@Test
+	public void test_BASIS_REAL() {
+
+		Function<BigDecimal, BigDecimal> f = Functions.BASIS_REAL;
+
+		assertNull(f.apply(null));
+
+		assertEquals(new BigDecimal("0.0010"), f.apply(BigDecimal.TEN));
 
 	}
 
