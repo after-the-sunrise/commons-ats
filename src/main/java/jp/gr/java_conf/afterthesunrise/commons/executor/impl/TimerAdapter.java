50e2bbb1e637b0d7b461a122a088d83bf487d1b2
Takanori Takase <subac1008-git@yahoo.co.jp>
2013-03-07 23:42:47 +0900

    Added timer adapter implementation.

---
+++ src/main/java/jp/gr/java_conf/afterthesunrise/commons/executor/impl/TimerAdapter.java
@@ -0,0 +1,57 @@
+package jp.gr.java_conf.afterthesunrise.commons.executor.impl;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicLong;
+
+import jp.gr.java_conf.afterthesunrise.commons.bean.Initializable;
+
+/**
+ * @author takanori.takase
+ */
+public class TimerAdapter extends TimerTask implements Initializable, Closeable {
+
+	private static final String NAME = TimerAdapter.class.getSimpleName() + "-";
+
+	private static final AtomicLong COUNT = new AtomicLong();
+
+	private final Runnable runnable;
+
+	private final Timer timer;
+
+	private final long interval;
+
+	public TimerAdapter(Runnable r, long interval) {
+		this(r, interval, new Timer(NAME + COUNT.incrementAndGet(), true));
+	}
+
+	public TimerAdapter(Runnable r, long interval, Timer timer) {
+
+		this.runnable = checkNotNull(r);
+
+		this.interval = interval;
+
+		this.timer = checkNotNull(timer);
+
+	}
+
+	@Override
+	public void initialize() throws IOException {
+		timer.schedule(this, interval, interval);
+	}
+
+	@Override
+	public void close() throws IOException {
+		timer.cancel();
+	}
+
+	@Override
+	public void run() {
+		runnable.run();
+	}
+
+}
---
+++ src/test/java/jp/gr/java_conf/afterthesunrise/commons/executor/impl/TimerAdapterTest.java
@@ -0,0 +1,53 @@
+package jp.gr.java_conf.afterthesunrise.commons.executor.impl;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import com.google.common.io.Closeables;
+
+/**
+ * @author takanori.takase
+ */
+public class TimerAdapterTest {
+
+	@Test(timeout = 5000L)
+	public void testSequence() throws Exception {
+
+		final CountDownLatch latch = new CountDownLatch(5);
+
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+
+				Thread t = Thread.currentThread();
+
+				String name = t.getName();
+
+				assertTrue(name, name.startsWith("TimerAdapter-"));
+
+				assertTrue(t.isDaemon());
+
+				latch.countDown();
+
+			}
+		};
+
+		TimerAdapter target = new TimerAdapter(r, 10L);
+
+		try {
+
+			target.initialize();
+
+			assertTrue(latch.await(6000L, TimeUnit.MILLISECONDS));
+
+		} finally {
+			Closeables.closeQuietly(target);
+		}
+
+	}
+
+}
