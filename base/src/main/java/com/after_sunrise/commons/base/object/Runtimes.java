448d0d48762caef802de894d0ba60def759766d0
Takanori Takase <subac1008-git@yahoo.co.jp>
2013-07-18 19:31:15 +0900

    Added retrieval methods.

--- base/src/main/java/com/after_sunrise/commons/base/object/Runtimes.java
+++ base/src/main/java/com/after_sunrise/commons/base/object/Runtimes.java
@@ -2,6 +2,7 @@ package com.after_sunrise.commons.base.object;
 
 import java.io.File;
 import java.lang.management.ManagementFactory;
+import java.math.BigDecimal;
 
 /**
  * @author takanori.takase
@@ -38,12 +39,106 @@ public final class Runtimes {
 		return PROCESS_ID;
 	}
 
+	public static final String getStringProperty(String key) {
+
+		if (key == null || key.isEmpty()) {
+			return null;
+		}
+
+		return System.getProperty(key);
+
+	}
+
+	public static final Boolean getBooleanProperty(String key) {
+
+		String value = getStringProperty(key);
+
+		return Boolean.valueOf(value);
+
+	}
+
+	public static final Integer getIntProperty(String key) {
+
+		String value = getStringProperty(key);
+
+		Integer val;
+
+		try {
+			val = Integer.valueOf(value);
+		} catch (Exception e) {
+			val = null;
+		}
+
+		return val;
+
+	}
+
+	public static final Long getLongProperty(String key) {
+
+		String value = getStringProperty(key);
+
+		Long val;
+
+		try {
+			val = Long.valueOf(value);
+		} catch (Exception e) {
+			val = null;
+		}
+
+		return val;
+
+	}
+
+	public static final Double getDoubleProperty(String key) {
+
+		String value = getStringProperty(key);
+
+		Double val;
+
+		try {
+			val = Double.valueOf(value);
+		} catch (Exception e) {
+			val = null;
+		}
+
+		return val;
+
+	}
+
+	public static final BigDecimal getDecimalProperty(String key) {
+
+		String value = getStringProperty(key);
+
+		BigDecimal val;
+
+		try {
+			val = new BigDecimal(value);
+		} catch (Exception e) {
+			val = null;
+		}
+
+		return val;
+
+	}
+
+	public static final File getFileProperty(String key) {
+
+		String value = getStringProperty(key);
+
+		if (value == null || value.isEmpty()) {
+			return null;
+		}
+
+		return new File(value);
+
+	}
+
 	public static String getUserName() {
-		return System.getProperty("user.name", null);
+		return getStringProperty("user.name");
 	}
 
 	public static File getUserHome() {
-		return new File(System.getProperty("user.home", null));
+		return getFileProperty("user.home");
 	}
 
 }
--- base/src/test/java/com/after_sunrise/commons/base/object/RuntimesTest.java
+++ base/src/test/java/com/after_sunrise/commons/base/object/RuntimesTest.java
@@ -1,6 +1,7 @@
 package com.after_sunrise.commons.base.object;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
@@ -9,13 +10,36 @@ import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
+import java.math.BigDecimal;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * @author takanori.takase
  */
 public class RuntimesTest {
+
+	private static final String KEY = RuntimesTest.class.getName();
+
+	@BeforeClass
+	public static void setUpBefore() {
+
+		System.setProperty(KEY, "123");
+
+		System.setProperty(KEY + "_empty", "");
+
+	}
+
+	@AfterClass
+	public static void tearDownAfter() {
+
+		System.clearProperty(KEY);
+
+		System.clearProperty(KEY + "_empty");
+
+	}
 
 	@Test(expected = IllegalAccessError.class)
 	public void testConstructor() throws Throwable {
@@ -57,6 +81,86 @@ public class RuntimesTest {
 		Long act = Runtimes.retrieveProcessId("*");
 
 		assertNull(act);
+
+	}
+
+	@Test
+	public void testGetStringProperty() {
+
+		assertNull(Runtimes.getStringProperty(null));
+
+		assertNull(Runtimes.getStringProperty(""));
+
+		assertNull(Runtimes.getStringProperty(KEY + "_FOO"));
+
+		assertEquals("123", Runtimes.getStringProperty(KEY));
+
+	}
+
+	@Test
+	public void testGetBooleanProperty() {
+
+		assertFalse(Runtimes.getBooleanProperty(null));
+
+		assertFalse(Runtimes.getBooleanProperty(""));
+
+		assertFalse(Runtimes.getBooleanProperty(KEY + "_FOO"));
+
+		assertFalse(Runtimes.getBooleanProperty(KEY));
+
+	}
+
+	@Test
+	public void testGetIntProperty() {
+
+		assertNull(Runtimes.getIntProperty(null));
+
+		assertNull(Runtimes.getIntProperty(""));
+
+		assertNull(Runtimes.getIntProperty(KEY + "_FOO"));
+
+		assertEquals((Integer) 123, Runtimes.getIntProperty(KEY));
+
+	}
+
+	@Test
+	public void testGetLongProperty() {
+
+		assertNull(Runtimes.getLongProperty(null));
+
+		assertNull(Runtimes.getLongProperty(""));
+
+		assertNull(Runtimes.getLongProperty(KEY + "_FOO"));
+
+		assertEquals((Long) 123L, Runtimes.getLongProperty(KEY));
+
+	}
+
+	@Test
+	public void testGetDecimalProperty() {
+
+		assertNull(Runtimes.getDecimalProperty(null));
+
+		assertNull(Runtimes.getDecimalProperty(""));
+
+		assertNull(Runtimes.getDecimalProperty(KEY + "_FOO"));
+
+		assertEquals(new BigDecimal("123"), Runtimes.getDecimalProperty(KEY));
+
+	}
+
+	@Test
+	public void testGetFileProperty() {
+
+		assertNull(Runtimes.getFileProperty(null));
+
+		assertNull(Runtimes.getFileProperty(""));
+
+		assertNull(Runtimes.getFileProperty(KEY + "_FOO"));
+
+		assertNull(Runtimes.getFileProperty(KEY + "_empty"));
+
+		assertEquals("123", Runtimes.getFileProperty(KEY).getPath());
 
 	}
 
