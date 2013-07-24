4c4a78b8c927cfdeec1452c85745ddcb72f4d063
Takanori Takase <subac1008-git@yahoo.co.jp>
2013-07-25 06:33:11 +0900

    Migrated sounds to independent module.

--- ui/src/main/java/com/after_sunrise/commons/ui/object/Sounds.java
+++ ui/src/main/java/com/after_sunrise/commons/ui/object/Sounds.java
@@ -1,13 +1,17 @@
-package com.after_sunrise.commons.base.object;
+package com.after_sunrise.commons.ui.object;
 
+import java.awt.Toolkit;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+
+import com.after_sunrise.commons.base.object.IOs;
 
 /**
  * @author takanori.takase
@@ -16,6 +20,44 @@ public final class Sounds {
 
 	private Sounds() {
 		throw new IllegalAccessError("Utility class shouldn't be instantiated.");
+	}
+
+	public static final String ENABLE_TOOLKIT = "sounds.toolkit.enabled";
+
+	static AtomicReference<Toolkit> TOOLKIT;
+
+	static Toolkit getToolkit() {
+
+		AtomicReference<Toolkit> ref = TOOLKIT;
+
+		if (ref == null) {
+
+			String enabled = System.getProperty(ENABLE_TOOLKIT);
+
+			if (Boolean.valueOf(enabled)) {
+				ref = new AtomicReference<Toolkit>(Toolkit.getDefaultToolkit());
+			} else {
+				ref = new AtomicReference<Toolkit>(null);
+			}
+
+			TOOLKIT = ref;
+
+		}
+
+		return ref.get();
+
+	}
+
+	public static void beep() {
+
+		Toolkit toolkit = getToolkit();
+
+		if (toolkit == null) {
+			return;
+		}
+
+		toolkit.beep();
+
 	}
 
 	public static Clip getClip(byte[] bytes) throws IOException {
---
+++ ui/src/test/java/com/after_sunrise/commons/ui/object/SoundsTest.java
@@ -0,0 +1,169 @@
+package com.after_sunrise.commons.ui.object;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.net.URL;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.sound.sampled.Clip;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.after_sunrise.commons.base.object.Streams;
+
+/**
+ * @author takanori.takase
+ */
+public class SoundsTest {
+
+	private byte[] sample;
+
+	@Before
+	public void setUp() throws Exception {
+
+		URL url = getClass().getClassLoader().getResource("sample.wav");
+
+		InputStream in = url.openStream();
+
+		try {
+			sample = Streams.getBytes(in);
+		} finally {
+			in.close();
+		}
+
+		Sounds.TOOLKIT = null;
+
+		System.clearProperty(Sounds.ENABLE_TOOLKIT);
+
+	}
+
+	@After
+	public void tearDown() {
+
+		Sounds.TOOLKIT = null;
+
+		System.clearProperty(Sounds.ENABLE_TOOLKIT);
+
+	}
+
+	@Test(expected = IllegalAccessError.class)
+	public void testConstructor() throws Throwable {
+
+		Class<?> clazz = Sounds.class;
+
+		Constructor<?> c = clazz.getDeclaredConstructor();
+
+		assertTrue(Modifier.isPrivate(c.getModifiers()));
+
+		c.setAccessible(true);
+
+		try {
+			c.newInstance();
+		} catch (InvocationTargetException e) {
+			throw e.getCause();
+		}
+
+	}
+
+	@Test
+	public void testGetToolkit() {
+
+		System.setProperty(Sounds.ENABLE_TOOLKIT, "true");
+
+		Toolkit t = Sounds.getToolkit();
+		assertSame(Toolkit.getDefaultToolkit(), t);
+
+		assertSame(t, Sounds.getToolkit());
+		assertSame(t, Sounds.getToolkit());
+		assertSame(t, Sounds.getToolkit());
+
+	}
+
+	@Test
+	public void testGetToolkit_Disabled() {
+
+		assertNull(Sounds.getToolkit());
+		assertNull(Sounds.getToolkit());
+		assertNull(Sounds.getToolkit());
+
+	}
+
+	@Test
+	public void testBeep() {
+
+		Toolkit mock = mock(Toolkit.class);
+
+		Sounds.TOOLKIT = new AtomicReference<Toolkit>(mock);
+
+		Sounds.beep();
+
+		verify(mock).beep();
+
+	}
+
+	@Test
+	public void testBeep_Null() {
+
+		Sounds.TOOLKIT = new AtomicReference<Toolkit>(null);
+
+		Sounds.beep();
+
+		assertNull(Sounds.TOOLKIT.get());
+
+	}
+
+	@Test
+	public void testGetClip() throws IOException {
+
+		try {
+
+			Clip clip = Sounds.getClip(sample);
+
+			try {
+				assertNotNull(clip);
+			} finally {
+				clip.close();
+			}
+
+		} catch (IOException e) {
+			// Ignore as some platform may not like ".wav"
+		}
+
+		assertNull(Sounds.getClip(new byte[0]));
+
+		assertNull(Sounds.getClip(null));
+
+	}
+
+	@Test(expected = IOException.class)
+	public void testGetClip_Invalid() throws IOException {
+		Sounds.getClip(new byte[] { 0 });
+	}
+
+	@Test
+	public void testCloseQuietly() throws IOException {
+
+		Clip clip = mock(Clip.class);
+
+		Sounds.closeQuietly(clip);
+
+		verify(clip).close();
+
+		Sounds.closeQuietly(null);
+
+	}
+
+}
